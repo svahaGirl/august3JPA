eb821f8f2357eb85fcc08b0c6169851d04f32573
Ying - a buddhist developer <fyanton@hotmail.com>
2021-08-04 19:47:47 -0700

    Create August3Application.java
    Create August3Application.java

---
+++ August3Application.java
@@ -0,0 +1,16 @@
+package com.perscholas.august3;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+
+@SpringBootApplication
+public class August3Application {
+
+
+	public static void main(String[] args) {
+		SpringApplication.run(August3Application.class, args);
+	}
+
+
+}
